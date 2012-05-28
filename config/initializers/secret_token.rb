d10e53665b265007b99edb4a84c92d7954cc7989
Manuel Nunez <mana2009@unitec.edu>
2012-05-28 08:35:13 -0600

    creando examen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codigo::Application.config.secret_token = 'd07de291281a79649557b53433248ee7904b983aef404666ad78bb5a03a2af574046e76c560cff5884e683f9c5de876839fa8f15f1252184e23b825b036ff7cf'
